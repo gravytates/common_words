6e65260b3071c0d39acd273d1ba8001a9fa7bd1f
Grady Shelton <gradyish@gmail.com>
2019-10-13 11:40:30 -0700

    refactor solution to a class with method and have it execute at the end of the file. add test file where tests will be housed and ran.

--- common_three_words.rb
+++ common_three_words.rb
@@ -1,29 +1,39 @@
-def format_word(word)
-  word.downcase.gsub(/[^a-z0-9\s+]?/i, "")
-end
+#!/usr/bin/env ruby
 
-areArgumentsPresent = !ARGV.empty?
-files = areArgumentsPresent ? ARGV : ARGF
-three_word_frequency_hash = Hash.new(0)
-for file in files
-  words = ""
-  areArgumentsPresent ? File.open(file).each { |line| words += line } : words = file
-  words = words.split(' ') 
-  words.each_with_index do |word, index|
-    if words[index+2].nil?
-     next 
+class WordFrequency
+  def self.top_word_triplets
+    areArgumentsPresent = !ARGV.empty?
+    files = areArgumentsPresent ? ARGV : ARGF
+    three_word_frequency_hash = Hash.new(0)
+
+    for file in files
+      words = ""
+      areArgumentsPresent ? File.open(file).each { |line| words += line } : words = file
+      delimiters = [/\s/, "\n", '\n']
+      words = words.split(Regexp.union(delimiters))
+        .map {|w| format_word(w)}
+        .delete_if {|w| w.length == 0}
+      words.each_with_index do |word, index|
+        if words[index+2].nil?
+        next 
+        end
+        three_words = [word, words[index+1], words[index+2]]
+        three_word_frequency_hash[three_words] += 1
+      end
     end
-    first = format_word(word)
-    second = format_word(words[index+1])
-    third = format_word(words[index+2])
-    three_words = [first, second, third]
-    three_word_frequency_hash[three_words] += 1
-  end  
+
+    top_hundred_word_triplets = three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
+
+    top_hundred_word_triplets.each do |key,value|
+      words = key.join(' ')
+      puts "#{value} - #{words},"
+    end
+  end
+
+  private
+    def self.format_word(word)
+      word.downcase.gsub(/[^a-z0-9\s]?/i, "")
+    end
 end
 
-top_hundred_word_triplets = three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
-
-top_hundred_word_triplets.each do |key,value|
-  words = key.join(' ')
-  puts "#{value} - #{words},"
-end
+WordFrequency.top_word_triplets
---
+++ common_three_words_tests.rb
@@ -0,0 +1,4 @@
+gem 'test-unit'
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+
