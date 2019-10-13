016570a9b5783b76e188d44cda966f9880bdd823
Grady Shelton <gradyish@gmail.com>
2019-10-12 18:24:38 -0700

    store all word triplets in a frequency hash, and list the top 100 most common word triplets.

--- common_three_words.rb
+++ common_three_words.rb
@@ -1,10 +1,29 @@
-files = !ARGV.empty? ? ARGV : ARGF
+def format_word(word)
+  word.downcase.gsub(/[^a-z0-9\s+]?/i, "")
+end
+
+areArgumentsPresent = !ARGV.empty?
+files = areArgumentsPresent ? ARGV : ARGF
+three_word_frequency_hash = Hash.new(0)
 for file in files
   words = ""
-  !ARGV.empty? ? File.open(file).each { |line| words += line } : words = file
-  words = words.split(' ')
-  words.each do |word|
-    formatted_word = word.downcase.gsub(/[^a-z0-9\s]/i, "")
-    puts formatted_word + " " 
+  areArgumentsPresent ? File.open(file).each { |line| words += line } : words = file
+  words = words.split(' ') 
+  words.each_with_index do |word, index|
+    if words[index+2].nil?
+     next 
+    end
+    first = format_word(word)
+    second = format_word(words[index+1])
+    third = format_word(words[index+2])
+    three_words = [first, second, third]
+    three_word_frequency_hash[three_words] += 1
   end  
+end
+
+top_hundred_word_triplets = three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
+
+top_hundred_word_triplets.each do |key,value|
+  words = key.join(' ')
+  puts "#{value} - #{words},"
 end
