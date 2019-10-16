9e03c68cd09fb91b66d3f568e8f236637081d715
Grady Shelton <gradyish@gmail.com>
2019-10-15 18:17:28 -0700

    add first test. return result in addition to printing to terminal.

--- common_three_words.rb
+++ common_three_words.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 
 class WordFrequency
-  def self.top_word_triplets
-    areArgumentsPresent = !ARGV.empty?
-    files = areArgumentsPresent ? ARGV : ARGF
+  def self.top_word_triplets(*files)
+    hasArguments = !ARGV.empty? || files.length > 0
+    @files
+    if hasArguments
+      files.length > 0 ? @files = files : @files = ARGV
+    elsif ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
+      @files = ARGF
+    else 
+      return puts "No file found."
+    end
     words = ""
     three_word_frequency_hash = Hash.new(0)
     delimiters = [/\s/, "\n", '\n']
  
-    files.each do |file| 
-      if areArgumentsPresent 
+    @files.each do |file| 
+      if hasArguments 
         File.readlines(file).each { |line| words += line }
       else 
         words += file
@@ -30,16 +37,22 @@ class WordFrequency
 
     top_hundred_word_triplets = three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
 
+    @result = ""
     top_hundred_word_triplets.each do |key,value|
       words = key.join(' ')
-      puts "#{value} - #{words},"
+      @result += "#{value} - #{words}, \n"
     end
+
+    # print to terminal
+    print @result
+    # return result for testing, other uses
+    return @result
   end
 
-  private
-    def self.format_word(word)
-      word.downcase.gsub(/[^a-z0-9\s]?/i, "")
-    end
+  
+  def self.format_word(word)
+    word.downcase.gsub(/[^a-z0-9\s]?/i, "")
+  end
 end
 
 WordFrequency.top_word_triplets
--- common_three_words_tests.rb
+++ common_three_words_tests.rb
@@ -1,14 +1,66 @@
 gem 'test-unit'
 require 'test/unit'
 require 'test/unit/ui/console/testrunner'
+require './common_three_words'
+
 
 # Tests
-# class DeserializerTest < Test::Unit::TestCase
-#   def test_deserialize_array()
-#     assert_equal([10, 13, 1].deserialize_array, [10, 13, 1])
-#     assert_equal([10, [13, 1], 5].deserialize_array, [10, 13, 1, 5])
-#     assert_equal([10, [13, 1, [5, 6], 7], 8, 9].deserialize_array, [10, 13, 1, 5, 6, 7, 8, 9])
-#     assert_equal([].deserialize_array, [])
-#     assert_equal([[[], []], [[[]]]].deserialize_array, [])
-#   end
-# end
+class CommonThreeWordsTest < Test::Unit::TestCase
+  def test_common_three_words()
+    @test_file_one = File.open('./example1.txt')
+    @test_file_two = File.open('./example2.txt')
+    @origin_of_species = File.open('./origin-of-species.txt')
+    assert_equal(WordFrequency.top_word_triplets(@test_file_one), 
+      "6 - one two three, \n" +
+      "5 - three one two, \n" +
+      "5 - two three one, \n" +
+      "3 - four five six, \n" +
+      "2 - six four five, \n" +
+      "2 - five six four, \n" +
+      "2 - bla bla bla, \n" +
+      "1 - are my wayward, \n" +
+      "1 - we are my, \n" +
+      "1 - here we are, \n" +
+      "1 - so here we, \n" +
+      "1 - document so here, \n" +
+      "1 - a document so, \n" +
+      "1 - in a document, \n" +
+      "1 - words in a, \n" +
+      "1 - some words in, \n" +
+      "1 - need some words, \n" +
+      "1 - just need some, \n" +
+      "1 - i just need, \n" +
+      "1 - because i just, \n" +
+      "1 - here because i, \n" +
+      "1 - type here because, \n" +
+      "1 - i type here, \n" +
+      "1 - what i type, \n" +
+      "1 - matter what i, \n" +
+      "1 - really matter what, \n" +
+      "1 - doesnt really matter, \n" +
+      "1 - bla doesnt really, \n" +
+      "1 - bla bla doesnt, \n" +
+      "1 - hello darkness my, \n" +
+      "1 - five six bla, \n" +
+      "1 - three four five, \n" +
+      "1 - two three four, \n" +
+      "1 - son one two, \n" +
+      "1 - wayward son one, \n" +
+      "1 - my wayward son, \n" +
+      "1 - again bla bla, \n" +
+      "1 - see again bla, \n" +
+      "1 - and see again, \n" +
+      "1 - here and see, \n" +
+      "1 - you here and, \n" +
+      "1 - see you here, \n" +
+      "1 - friend see you, \n" +
+      "1 - old friend see, \n" +
+      "1 - my old friend, \n" +
+      "1 - darkness my old, \n" +
+      "1 - six bla bla, \n")
+    @test_file_one.close
+    @test_file_two.close
+    @origin_of_species.close
+  end
+end
+
