e04a607f9b05cc60e6c8ccfd92f6a110e9203601
Grady Shelton <gradyish@gmail.com>
2019-10-16 16:01:33 -0700

    add standard input tests. add comments for test file and script.

--- common_three_words.rb
+++ common_three_words.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/env ruby
 
 class WordFrequency
+  # method to return 100 most common word triplets.
   def self.top_word_triplets(*files)
+
+    # identify if arguments are present from command line or via test/other invocation.
     hasArguments = !ARGV.empty? || files.length > 0
     @files
+    # set files variable to an array of files or to a given standard input.
     if hasArguments
       files.length > 0 ? @files = files : @files = ARGV
     elsif ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
@@ -11,10 +15,12 @@ class WordFrequency
     else 
       return puts "No file found."
     end
-    words = ""
+    words = String.new
     three_word_frequency_hash = Hash.new(0)
+    # common unix and dos delimiters.
     delimiters = [/\s/, "\n", '\n', "\r\n", '\r\n']
  
+    # create a string of words from the files.
     @files.each do |file| 
       if hasArguments && file
         File.readlines(file).each { |line| 
@@ -27,10 +33,12 @@ class WordFrequency
       end
     end
 
+    # split and format words based on requirements, respecting new lines and stripping punctuation.
     formatted_words = words.split(Regexp.union(delimiters))
       .map {|w| format_word(w)}
       .delete_if {|w| w.length == 0}
 
+    # create three word hash with frequency.
     formatted_words.each_with_index do |word, index|
       if formatted_words[index+2].nil?
         next 
@@ -39,23 +47,26 @@ class WordFrequency
       three_word_frequency_hash[three_words] += 1
     end  
 
+    # sort the hash.
     top_hundred_word_triplets = three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
 
+    # format result.
     @result = String.new
     top_hundred_word_triplets.each do |key,value|
       words = key.join(' ')
       @result += "#{value} - #{words}, "
     end
-    # print to terminal
+    # print to terminal.
     print @result
-    # return @result for testing, other usese
+    # return @result for testing, other uses.
     @result
   end
 
-  
+  # helper funciton to remove punctuation.
   def self.format_word(word)
     word.downcase.gsub(/[^a-z0-9\s]?/i, "")
   end
 end
 
+# execute class method when file is ran.
 WordFrequency.top_word_triplets
