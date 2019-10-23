b23123af5b2c5643a9db7bd0d33109093b721298
Grady Shelton <gradyish@gmail.com>
2019-10-23 15:17:16 -0700

    more efficient O notation (less looping), better instance variable naming.

--- common_three_words.rb
+++ common_three_words.rb
@@ -1,60 +1,65 @@
 #!/usr/bin/env ruby
+require 'pry'
 
 class WordFrequency
   # method to return 100 most common word triplets.
   def self.top_word_triplets(*files)
 
     # identify if arguments are present from command line or via test/other invocation.
-    hasArguments = !ARGV.empty? || files.length > 0
+    @hasArguments = !ARGV.empty? || files.length > 0
     @files
     # set files variable to an array of files or to a given standard input.
-    if hasArguments
+    if @hasArguments
       files.length > 0 ? @files = files : @files = ARGV
     elsif ARGF.filename != "-" or (not STDIN.tty? and not STDIN.closed?)
       @files = ARGF
     else 
       return puts "No file found."
     end
-    words = String.new
-    three_word_frequency_hash = Hash.new(0)
+    @words = String.new
+    @three_word_frequency_hash = Hash.new(0)
     # common unix and dos delimiters.
-    delimiters = [/\s/, "\n", '\n', "\r\n", '\r\n']
+    @delimiters = [/\s/, " ", "\n", '\n', "\r\n", '\r\n']
  
     # create a string of words from the files.
     @files.each do |file| 
-      if hasArguments && file
+      if @hasArguments && file
         File.readlines(file).each { |line| 
           if line 
-            words += line.to_s 
+            @words += line.to_s 
           end
         }
       else 
-        words += file.to_s
+        @words += file.to_s
       end
     end
 
     # split and format words based on requirements, respecting new lines and stripping punctuation.
-    formatted_words = words.split(Regexp.union(delimiters))
-      .map {|w| format_word(w)}
-      .delete_if {|w| w.length == 0}
-
-    # create three word hash with frequency.
-    formatted_words.each_with_index do |word, index|
-      if formatted_words[index+2].nil?
-        next 
+    # create three word triplets and add to the three_word_frequency_hash, counting frequencies.
+    @current_three_words = Array.new
+    @words.split(Regexp.union(@delimiters)).each do |word|
+      formatted_word = format_word(word)
+      if formatted_word.length == 0
+        next
       end
-      three_words = [word, formatted_words[index+1], formatted_words[index+2]]
-      three_word_frequency_hash[three_words] += 1
-    end  
+      @current_three_words.push formatted_word
+      if @current_three_words.length > 3
+        @current_three_words.shift
+      elsif @current_three_words.length < 3
+        next
+      end
+      # map to new array in order to prevent keys from being referenced to mutating array.
+      @three_words = @current_three_words.map{|w| w}
+      @three_word_frequency_hash[@three_words] += 1
+    end
 
-    # sort the hash.
-    top_hundred_word_triplets = three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
+    @top_hundred_word_triplets = @three_word_frequency_hash.sort_by {|k,v| v}.reverse.first(100)
 
     # format result.
     @result = String.new
-    top_hundred_word_triplets.each do |key,value|
-      words = key.join(' ')
-      @result += "#{value} - #{words}, "
+    @top_hundred_word_triplets.each do |key,value|
+      @word_triplet = key.join(' ')
+      @result += "#{value} - #{@word_triplet}, "
     end
     # print to terminal.
     print @result
